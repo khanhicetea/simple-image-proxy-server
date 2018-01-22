c2202ee874ddc1247426c12ebc3b712aaed852be
khanhicetea <khanhicetea@gmail.com>
2018-01-22 11:56:55 +0700

    Fixed !

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Powered by [intervention/image](http://image.intervention.io/)
 
 ## Guide
 
+Example URL : `http://[your-domain-name]/index.php?s=7E0D0626111602763FAE22A905A5096F&w=413&h=640&url=aHR0cHM6Ly9pLmltZ3VyLmNvbS9xNE1xUS5qcGc=`
+
 - URL query params
     + `url` : base64_encode of remote url
-    + `w` : width of returned image
-    + `h` : height of  returned image
+    + `w` : width of returned image (optional if height is set)
+    + `h` : height of  returned image (optional if width is set)
     + `s` : signature to verify origin
 - `storage` directory contains 2 sub-dirs:
     + `image` : storage downloaded image from remote resource
--- index.php
+++ index.php
@@ -11,8 +11,8 @@ if ($secret && md5($_GET['url'].$secret) != strtolower($_GET['s'])) {
 $url = base64_decode($_GET['url']);
 $parts = explode('.', $url);
 $ext = strtolower($parts[count($parts)-1]);
-$w = intval($_GET['w']);
-$h = intval($_GET['h']);
+$w = empty($_GET['w']) ? null : intval($_GET['w']);
+$h = empty($_GET['h']) ? null : intval($_GET['h']);
 $md5 = md5($url);
 $hash = md5($url.$w.$h);
 $storage_image = __DIR__.'/storage/image';
@@ -36,6 +36,8 @@ if (!file_exists($img_path)) {
     download_via_cURL($url, $img_path);
 }
 
-$image = $manager->make($img_path)->resize($w, $h);
+$image = $manager->make($img_path)->resize($w, $h, function ($constraint) {
+    $constraint->aspectRatio();
+});
 $image->save($hash_path);
 echo $image->response();
